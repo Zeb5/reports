a05104916e15ed9a330b49c9a9cd480bc1b51a1a
Zeb5 <blackburn.ze@oundleschool.org.uk>
2024-11-26 09:20:20 +0000

    updated stuff

---
+++ addusers.php
@@ -0,0 +1,30 @@
+<?php
+include_once("connection.php");
+print_r($_POST);
+echo($_POST["forename"]);
+switch($_POST["role"]){
+	case "Pupil":
+		$role=0;
+		break;
+	case "Teacher":
+		$role=1;
+		break;
+	case "Admin":
+		$role=2;
+		break;
+}
+$stmt = $conn->prepare("INSERT INTO TblUser (UserID,Gender,Surname,Forename,Password,House,Year ,Role)
+VALUES (null,:gender,:surname,:forename,:password,:house,:year,:role)");
+
+
+$stmt->bindParam(':forename', $_POST["forename"]);
+$stmt->bindParam(':surname', $_POST["surname"]);
+$stmt->bindParam(':house', $_POST["house"]);
+$stmt->bindParam(':year', $_POST["year"]);
+$stmt->bindParam(':password', $_POST["passwd"]);
+$stmt->bindParam(':gender', $_POST["gender"]);
+$stmt->bindParam(':role', $role);
+$stmt->execute();
+$conn=null;
+
+?>
---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "reports";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo"Connected Successfully";
+}
+catch(PDOException $e){
+    echo"Connection Failed: " . $e->getMessage();
+}
+?>
